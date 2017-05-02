f96dcd5a3e7d939082b86f78ebb71475322103a3
jhartman <jhartman@sfu.ca>
2017-05-02 11:08:39 -0400

    Add MNIST example

---
+++ Tutorail_MNIST/main.py
@@ -0,0 +1,43 @@
+## Import Tensorflow
+import tensorflow as tf
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
+
+
+# Create session
+sess = tf.InteractiveSession()
+
+# Placeholders
+x = tf.placeholder(tf.float32, shape=[None, 784])
+y_ = tf.placeholder(tf.float32, shape=[None, 10])
+
+# Variables
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+
+# Register Variables in Computational Graph
+sess.run(tf.global_variables_initializer())
+
+# Regression Model
+y = tf.matmul(x, W) + b
+
+# Cross entropy loss function
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+
+
+# Training
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+# Train data
+for _ in range(1000):
+    batch = mnist.train.next_batch(100)
+    train_step.run(feed_dict={x: batch[0], y_: batch[1]})  # Feed dict can be used to replace placeholders with data.
+
+
+
+# Determine correct prediction
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
